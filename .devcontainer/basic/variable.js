c2f1713f993b3715466086a0b659cfa6d67e89cd
AmanRawathere <ar533582@gmail.com>
2024-09-25 20:22:32 +0000

    datatypes

---
+++ .devcontainer/basic/variable.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail = "aman@gmail.com"
+let accountPassword = "12345"
+accountCity = "jaipur"
+let accountState = "rajasthan"
+
+// accountId = 2 
+
+accountEmail ="akr@gmail.com"
+
+console.log(accountEmail);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+ 
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
