a64222931c712a2deead4d77fced443be5ed34ea
AmanRawathere <ar533582@gmail.com>
2024-09-28 06:57:56 +0000

    comparision

---
+++ .devcontainer/basic/comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >=1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+
+
+// console.log("2">= 1);
+// console.log("02">= 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2"=== 2);
+// comparision convert null to a number, treating it as 0.
+// thats why(3) null>=0 is true and (1)null>0 is false.
+
